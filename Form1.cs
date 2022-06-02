facdbba5d083a31795a68aef7f7889880964f42a
Carl-Joseph-Alapide <carljosephalapide@gmail.com>
2022-06-02 17:38:41 +0800

    Icon Uploaded and Maximize Button Disabled

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
