89172519efbd026795334d6743bba75c5456c17c
Carl-Joseph-Alapide <carljosephalapide@gmail.com>
2022-06-03 14:12:22 +0800

    First try for winner results (did not work hehe)

--- Form1.cs
+++ Form1.cs
@@ -19,10 +19,24 @@ namespace TicTacToe
                 All.Text = "O";
         All.Enabled = false;
         x_turn = !x_turn;
+         //labeling the turns   
             if (x_turn)
                 lblturn.Text = "X Turn";
             else
                 lblturn.Text = "O Turn";
+            //checking the winnner
+            Boolean winner = false;
+                if ((btnL1 == btnL2) == (btnL1 == btnL3) && (All.Enabled))
+                winner = true;
+            else if ((btnC1 == btnC2) == (btnC1 == btnC3) && (All.Enabled))
+                winner = true;
+            else if ((btnR1 == btnR2) == (btnR1 == btnR3) && (All.Enabled))
+                    winner = true;
+            if (winner)
+                if (x_turn)
+                    MessageBox.Show("The winner is O","Congratulations");
+                else
+                    MessageBox.Show("The winner is X","Congratulations");
 
 
         }
@@ -39,6 +53,7 @@ namespace TicTacToe
 
         private void lblturn_Click(object sender, EventArgs e)
         {
+            
         }
     }
 }
